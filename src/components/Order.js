b2872b0447f644d812a9c13986d2169ec57f6d80
Andrew Owen <andrew.owen@willowtreeapps.com>
2018-01-24 13:58:51 -0500

    17. Displaying Order State with Firebase
    - Create Order Component
    - Learn about render functions
    - user .reduce and .map to get data out of props

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,57 @@
-import React from 'react';;
+import React from 'react'
+import PropTypes from 'prop-types'
+import { formatPrice } from '../helpers'
+
 
 class Order extends React.Component {
- 
-  render() {
+
+  static propTypes = {
+    order: PropTypes.object.isRequired,
+    fishes: PropTypes.object.isRequired
+  }
+
+  constructor() {
+    super();
+    this.renderOrder = this.renderOrder.bind(this)
+  }
+
+  renderOrder(key) {
+    const fish = this.props.fishes[key]
+    const count = this.props.order[key]
+
+    if(!fish || fish.status === 'unavailable') {
+      return <li key={key}>Sorry! {fish ? fish.name : 'fish'} is no longer available!</li>
+    }
+
     return (
-      <div>
-        <h2>Order</h2>
+      <li key={key}>
+        <span>{count}lbs {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  }
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if(isAvailable) {
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal
+    }, 0)
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            { formatPrice(total) }
+
+          </li>
+        </ul>
       </div>
     );
   }
