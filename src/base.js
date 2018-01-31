1c218bc5ffb76d639ef3ae410b7dbe1b6749e76c
Andrew Owen <andrew.owen@willowtreeapps.com>
2018-01-31 14:32:07 -0500

    18. Persisting our State with Firebase - create base for firebase connections - add firebase connection and state sync using componentWillMount

--- src/base.js
+++ src/base.js
@@ -1,14 +1,32 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCdgXxyNqCZEeY36Ss9i4jFMSUh7FdGi6M',
-  authDomain: 'catch-of-the-day-wes-bos.firebaseapp.com',
-  databaseURL: 'https://catch-of-the-day-wes-bos.firebaseio.com',
-});
+  apiKey: 'AIzaSyDeszFXq3RA8JxwsWcHrNjO23PuqToa3qY',
+  authDomain: 'catch-of-the-day-ebaaa.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-ebaaa.firebaseio.com'
+})
 
 const base = Rebase.createClass(firebaseApp.database());
 
 export { firebaseApp };
 
 export default base;
+
+
+
+// import Rebase from 're-base';
+// import firebase from 'firebase';
+
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: 'AIzaSyCdgXxyNqCZEeY36Ss9i4jFMSUh7FdGi6M',
+//   authDomain: 'catch-of-the-day-wes-bos.firebaseapp.com',
+//   databaseURL: 'https://catch-of-the-day-wes-bos.firebaseio.com',
+// });
+
+// const base = Rebase.createClass(firebaseApp.database());
+
+// export { firebaseApp };
+
+// export default base;
