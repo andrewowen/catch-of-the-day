f8c3cda4f24b65774b7d91d01d07881aeb45834f
Andrew Owen <andrew.owen@willowtreeapps.com>
2018-02-21 13:53:34 -0500

    21. Removing Items from State
    Did:
    - Added button to remove both Fishes from Inventory and Order
    - cleaned up syntax

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 class AddFishForm extends React.Component {
   static propTypes = {
-    addFish: PropTypes.func.isRequired
+    addFish: PropTypes.func.isRequired,
   }
 
   createFish(event) {
--- src/components/Order.js
+++ src/components/Order.js
@@ -2,41 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { formatPrice } from '../helpers'
 
-
 class Order extends React.Component {
-
   static propTypes = {
     order: PropTypes.object.isRequired,
-    fishes: PropTypes.object.isRequired
+    fishes: PropTypes.object.isRequired,
+    removeFromOrder: PropTypes.func.isRequired,
   }
 
   constructor() {
-    super();
+    super()
     this.renderOrder = this.renderOrder.bind(this)
   }
 
   renderOrder(key) {
     const fish = this.props.fishes[key]
     const count = this.props.order[key]
+    const removeButton = <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
 
-    if(!fish || fish.status === 'unavailable') {
-      return <li key={key}>Sorry! {fish ? fish.name : 'fish'} is no longer available!</li>
+    if (!fish || fish.status === 'unavailable') {
+      return (
+        <li key={key}>
+          Sorry! {fish ? fish.name : 'fish'} is no longer available! {removeButton}
+        </li>
+      )
     }
 
     return (
       <li key={key}>
-        <span>{count}lbs {fish.name}</span>
+        <span>
+          {count}lbs {fish.name} {removeButton}{' '}
+        </span>
         <span className="price">{formatPrice(count * fish.price)}</span>
       </li>
     )
   }
   render() {
-    const orderIds = Object.keys(this.props.order);
+    const orderIds = Object.keys(this.props.order)
     const total = orderIds.reduce((prevTotal, key) => {
-      const fish = this.props.fishes[key];
-      const count = this.props.order[key];
-      const isAvailable = fish && fish.status === 'available';
-      if(isAvailable) {
+      const fish = this.props.fishes[key]
+      const count = this.props.order[key]
+      const isAvailable = fish && fish.status === 'available'
+      if (isAvailable) {
         return prevTotal + (count * fish.price || 0)
       }
       return prevTotal
@@ -48,13 +54,12 @@ class Order extends React.Component {
           {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
-            { formatPrice(total) }
-
+            {formatPrice(total)}
           </li>
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default Order;
+export default Order
